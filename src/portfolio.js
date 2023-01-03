66b33e00720db70dfa8886a3832581bd0ad8448c
Ilham <i.ramadhan@mediaagentur-in.berlin>
2023-01-03 14:58:42 +0100

    edit description

--- src/portfolio.js
+++ src/portfolio.js
@@ -9,7 +9,7 @@ const about = {
   name: 'Ilham Akbar Ramadhan',
   role: 'Junior IT Project Manager',
   description:
-    'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
+    'This is a Description',
   resume: 'https://example.com',
   social: {
     linkedin: 'https://linkedin.com',
